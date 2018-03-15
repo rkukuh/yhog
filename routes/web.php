3bfd31dc49ae30d27638e9ed6667c89b7f6ca08f
R. Kukuh <rkukuh@gmail.com>
2018-03-15 16:07:11 +0700

    Use shortcode

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
 Auth::routes();
-
+Route::view('/', 'welcome');
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/', function () {
-    return view('welcome');
-});
