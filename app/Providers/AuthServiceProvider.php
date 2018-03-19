458d86d61107157c07338a1ff18ae38411f3e9db
R. Kukuh <rkukuh@gmail.com>
2018-03-19 14:11:38 +0700

    Create policy for User

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\User;
 use App\Models\Admin\Post;
 use App\Policies\PostPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -15,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+        User::class => UserPolicy::class,
         Post::class => PostPolicy::class,
     ];
 
