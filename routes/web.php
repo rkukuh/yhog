0b83879853b5fbb2862f9139f81b3283cb8cd100
R. Kukuh <rkukuh@gmail.com>
2018-03-19 13:43:17 +0700

    Define route for Post on admin area

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,19 @@
 Auth::routes();
 Route::view('/', 'welcome');
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+////////////////////////////// ADMIN AREA //////////////////////////////
+
+Route::group([
+
+    'name'          => 'admin',
+    'prefix'        => 'admin',
+    'namespace'     => 'Admin',
+    'middleware'    => 'auth'
+
+], function () {
+
+    Route::resource('post', 'PostController');
+
+});
