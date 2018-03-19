e883b16a103ce7b1934acbf3227aeb0739baf98a
R. Kukuh <rkukuh@gmail.com>
2018-03-19 13:42:19 +0700

    Create and register policy for Post

---
+++ app/Policies/Post.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\ModelsAdminPost;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class Post
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the modelsAdminPost.
+     *
+     * @param  \App\User  $user
+     * @param  \App\ModelsAdminPost  $modelsAdminPost
+     * @return mixed
+     */
+    public function view(User $user, ModelsAdminPost $modelsAdminPost)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create modelsAdminPosts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the modelsAdminPost.
+     *
+     * @param  \App\User  $user
+     * @param  \App\ModelsAdminPost  $modelsAdminPost
+     * @return mixed
+     */
+    public function update(User $user, ModelsAdminPost $modelsAdminPost)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the modelsAdminPost.
+     *
+     * @param  \App\User  $user
+     * @param  \App\ModelsAdminPost  $modelsAdminPost
+     * @return mixed
+     */
+    public function delete(User $user, ModelsAdminPost $modelsAdminPost)
+    {
+        //
+    }
+}
