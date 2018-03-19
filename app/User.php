ea724d2f5ac840f96d15721373f5dd4f2f6eb03d
R. Kukuh <rkukuh@gmail.com>
2018-03-19 17:19:07 +0700

    Fix: Method Illuminate\Database\Query\Builder::hasRole does not exist.

--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use HasRoles;
     use Notifiable;
     use SoftDeletes;
 
