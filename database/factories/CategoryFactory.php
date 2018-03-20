319630b8b87b51fc329e7015099d2d09d30309fc
R. Kukuh <rkukuh@gmail.com>
2018-03-20 14:08:35 +0700

    FIX: Incorrect factory name

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Models\Admin\Category;
+
+$factory->define(Category::class, function (Faker $faker) {
+
+    return [
+        //
+    ];
+    
+});
--- database/factories/ModelsAdminCategoryFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Models\Admin\Category::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
