9939582b3d08e339bb8f38cdc55e446564b2c024
R. Kukuh <rkukuh@gmail.com>
2018-03-20 15:27:18 +0700

    NEW: Table seeder and example data for Category

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleAndPermissionSeeder::class);
         $this->call(UsersTableSeeder::class);
 
+        $this->call(CategoriesTableSeeder::class);
+
         $this->call(PostsTableSeeder::class);
     }
 }
