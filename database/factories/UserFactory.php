de1a9a1cf3c10321fa042e509591eb3af6898df2
R. Kukuh <rkukuh@gmail.com>
2018-03-25 19:36:54 +0700

    Update User's factory logic

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,10 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
+
+    $first_name = $faker->firstName;
+    $last_name  = $faker->lastName; 
+    
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => $first_name . ' ' . $last_name,
+        'email' => strtolower($first_name . '.' . $last_name) . '@yhog.example',
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
+
 });
