4353ed1702d43a776d78b739247e1e729ee8958b
R. Kukuh <rkukuh@gmail.com>
2018-03-26 13:57:05 +0700

    FIX: Class 'App\Http\Controllers\Admin\Controller' not found

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Models\Admin\Category;
 use Illuminate\Http\Request;
+use App\Models\Admin\Category;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
