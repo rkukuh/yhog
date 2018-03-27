3d73a72b0db8694353532de053026ebf1733d2f5
R. Kukuh <rkukuh@gmail.com>
2018-03-28 00:59:44 +0700

    Highlight partner category's sidebar menu

--- public/js/common.js
+++ public/js/common.js
@@ -70,6 +70,11 @@ $(function() {
         $('ul.sidebar-menu > li.treeview > ul.treeview-menu > li > a[href="' + base_url + '/admin/category-event"]').parent().addClass('active');
     }
 
+    if (full_url.indexOf('admin/category-partner') > -1) {
+        $('ul.sidebar-menu > li.treeview > a[href="#partner"]').parent().addClass('active');
+        $('ul.sidebar-menu > li.treeview > ul.treeview-menu > li > a[href="' + base_url + '/admin/category-partner"]').parent().addClass('active');
+    }
+
     if (full_url.indexOf('admin/post') > -1) {
         $('ul.sidebar-menu > li.treeview > a[href="#post"]').parent().addClass('active');
         $('ul.sidebar-menu > li.treeview > ul.treeview-menu > li > a[href="' + base_url + '/admin/post"]').parent().addClass('active');
