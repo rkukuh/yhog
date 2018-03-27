2ab4e06d7f815390fbee19092c52c2ff652a532e
R. Kukuh <rkukuh@gmail.com>
2018-03-27 17:29:43 +0700

    Use global scope

--- app/Models/Admin/Category.php
+++ app/Models/Admin/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Admin;
 
+use \App\Scopes\OrderByColumn;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -17,4 +18,19 @@ class Category extends Model
     ];
 
     protected $dates = ['deleted_at'];
+
+
+    /******************************************* SCOPE *******************************************/
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new OrderByColumn('name'));
+    }
 }
