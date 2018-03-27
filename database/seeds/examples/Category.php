425fa19bd4222e5b966202d7ae2e88925a5842db
R. Kukuh <rkukuh@gmail.com>
2018-03-27 17:55:17 +0700

    Define logic to generate Category example data

--- database/seeds/examples/Category.php
+++ database/seeds/examples/Category.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Admin\Category as CategoryModel;
 
 class Category extends Seeder
 {
@@ -11,6 +12,12 @@ class Category extends Seeder
      */
     public function run()
     {
-        //
+        /** BLOG **/
+        
+        $parent_blog_category = factory(CategoryModel::class)->states('blog')->create();
+
+        factory(CategoryModel::class, rand(5, 10))->states('blog')->create([
+            'parent_id' => $parent_blog_category
+        ]);
     }
 }
