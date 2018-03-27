310dd3fe871a0d3a9cdf5be88c40b296cbe677f7
R. Kukuh <rkukuh@gmail.com>
2018-03-27 17:27:27 +0700

    Use soft delete

--- app/Models/Admin/Category.php
+++ app/Models/Admin/Category.php
@@ -3,8 +3,11 @@
 namespace App\Models\Admin;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 }
