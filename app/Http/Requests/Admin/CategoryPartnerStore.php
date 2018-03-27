619a7e9e34ace80f7be55cf28da4c95bce339864
R. Kukuh <rkukuh@gmail.com>
2018-03-28 01:09:43 +0700

    Define logic for partner category's store form request

--- app/Http/Requests/Admin/CategoryPartnerStore.php
+++ app/Http/Requests/Admin/CategoryPartnerStore.php
@@ -13,7 +13,7 @@ class CategoryPartnerStore extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return auth()->user()->hasRole('admin');
     }
 
     /**
@@ -24,7 +24,24 @@ class CategoryPartnerStore extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|unique:categories|min:3|max:30',
+            'parent_id' => 'nullable|exists:categories,id',
         ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+
+            $this->merge(['_of' => 'partner']);
+            $this->merge(['slug' => str_slug($this->name)]);
+            
+        });
     }
 }
