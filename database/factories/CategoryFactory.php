769181597d4932068a9f5d313d6f49c5d4f87f49
R. Kukuh <rkukuh@gmail.com>
2018-03-27 17:39:52 +0700

    Define logic for Category model factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,8 +5,13 @@ use App\Models\Admin\Category;
 
 $factory->define(Category::class, function (Faker $faker) {
 
+    $name = 'Category Test #' . $faker->unixTime();
+
     return [
-        //
+        'parent_id' => null,
+        'name' => $name,
+        'slug' => str_slug($name),
+        '_of' => $faker->randomElement(['post', 'partner', 'event', 'donation'])
     ];
     
 });
