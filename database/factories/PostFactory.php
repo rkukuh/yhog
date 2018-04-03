ddb56318eb06811060b6dcf6837c19a492063cd4
R. Kukuh <rkukuh@gmail.com>
2018-04-03 20:14:32 +0700

    Define property of Post model factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,12 +1,23 @@
 <?php
 
-use App\Models\Admin\Post;
+use App\User;
+use Carbon\Carbon;
+use App\Models\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
 
     return [
-        //
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'excerpt' => $faker->text,
+        'body' => $faker->text(3000),
+        'published_at' => $faker->randomElement([
+            null, 
+            Carbon::now()->addDay(rand(0, 3))
+        ])
     ];
     
 });
