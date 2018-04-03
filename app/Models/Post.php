c745825c518c26ae13b7b662b294b84c2b1afcc2
R. Kukuh <rkukuh@gmail.com>
2018-04-03 20:10:20 +0700

    Use soft delete trait

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
 
 
     /*************************************** RELATIONSHIP ****************************************/
