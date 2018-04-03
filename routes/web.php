9f92256ff2091fc11e798201c01c7d71b958572e
R. Kukuh <rkukuh@gmail.com>
2018-04-03 20:55:56 +0700

    Define route name prefix for admin/backend

--- routes/web.php
+++ routes/web.php
@@ -9,55 +9,59 @@ Route::redirect('/home', '/admin', 301);
 
 ////////////////////////////// ADMIN AREA //////////////////////////////
 
-Route::group([
-
-    'name'          => 'admin.',
-    'prefix'        => 'admin',
-    'namespace'     => 'Admin',
-    'middleware'    => 'auth'
-
-], function () {
-
-    /** DASHBOARD **/
-
-    Route::view('/', 'admin.dashboard.index')->name('dashboard.index');
+Route::name('admin.')->group(function () {
     
-    /** BLOG **/
+    Route::group([
 
-    Route::resource('post', 'PostController');
-
-    Route::resource('category-post', 'CategoryPostController', [
-        'parameters' => ['category-post' => 'category']
-    ]);
-
-    /** EVENT **/
-
-    Route::resource('category-event', 'CategoryEventController', [
-        'parameters' => ['category-event' => 'category']
-    ]);
-
-    /** GALLERY **/
-
-    Route::resource('category-gallery', 'CategoryGalleryController', [
-        'parameters' => ['category-gallery' => 'category']
-    ]);
-
-    /** PARTNER **/
-
-    Route::resource('category-partner', 'CategoryPartnerController', [
-        'parameters' => ['category-partner' => 'category']
-    ]);
-
-    /** DONATION **/
-
-    Route::resource('category-donation', 'CategoryDonationController', [
-        'parameters' => ['category-donation' => 'category']
-    ]);
-
-    /** SETTING **/
-
-    Route::resource('tag', 'TagController');
-
-    Route::resource('user', 'UserController');
+        'name'          => 'admin.',
+        'prefix'        => 'admin',
+        'namespace'     => 'Admin',
+        'middleware'    => 'auth'
+    
+    ], function () {
+    
+        /** DASHBOARD **/
+    
+        Route::view('/', 'admin.dashboard.index')->name('dashboard.index');
+        
+        /** BLOG **/
+    
+        Route::resource('post', 'PostController');
+    
+        Route::resource('category-post', 'CategoryPostController', [
+            'parameters' => ['category-post' => 'category']
+        ]);
+    
+        /** EVENT **/
+    
+        Route::resource('category-event', 'CategoryEventController', [
+            'parameters' => ['category-event' => 'category']
+        ]);
+    
+        /** GALLERY **/
+    
+        Route::resource('category-gallery', 'CategoryGalleryController', [
+            'parameters' => ['category-gallery' => 'category']
+        ]);
+    
+        /** PARTNER **/
+    
+        Route::resource('category-partner', 'CategoryPartnerController', [
+            'parameters' => ['category-partner' => 'category']
+        ]);
+    
+        /** DONATION **/
+    
+        Route::resource('category-donation', 'CategoryDonationController', [
+            'parameters' => ['category-donation' => 'category']
+        ]);
+    
+        /** SETTING **/
+    
+        Route::resource('tag', 'TagController');
+    
+        Route::resource('user', 'UserController');
+    
+    });
 
 });
