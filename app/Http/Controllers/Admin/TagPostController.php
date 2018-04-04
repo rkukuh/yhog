e986c79fa3d149c26c011a389918fa5913a2faa5
R. Kukuh <rkukuh@gmail.com>
2018-04-04 11:52:54 +0700

    Remove unused class imports

--- app/Http/Controllers/Admin/TagPostController.php
+++ app/Http/Controllers/Admin/TagPostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Tag;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\TagPostStore;
 use App\Http\Requests\Admin\TagPostUpdate;
