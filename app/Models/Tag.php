1b7243ccc35d690d87cedd613ec515ada50fc7d0
R. Kukuh <rkukuh@gmail.com>
2018-04-04 10:28:04 +0700

    Use global scope

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use \App\Scopes\OrderByColumn;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -12,4 +13,19 @@ class Tag extends Model
     protected $fillable = ['name'];
 
     protected $dates = ['deleted_at'];
+
+
+    /******************************************* SCOPE *******************************************/
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new OrderByColumn('name'));
+    }
 }
