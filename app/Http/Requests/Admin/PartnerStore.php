daf0ca0afdd3611e609d089b203ed0dd8859cec3
R. Kukuh <rkukuh@gmail.com>
2018-04-05 15:49:15 +0700

    Define logic for partner's store form request

--- app/Http/Requests/Admin/PartnerStore.php
+++ app/Http/Requests/Admin/PartnerStore.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Admin;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PartnerStore extends FormRequest
@@ -13,7 +14,7 @@ class PartnerStore extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return auth()->user()->hasRole('admin');
     }
 
     /**
@@ -24,7 +25,56 @@ class PartnerStore extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|min:3|max:255',
+            'body' => 'required|min:5',
+            'excerpt' => 'required|min:5',
+            'category_id' => 'required|exists:categories,id',
+            'tag_id' => 'exists:tags,id',
+            'images' => 'required',
+            'images.*.image' => 'mimes:jpeg,png|min:50|max:1000'
         ];
+    }
+
+    /**
+     * Set custom validation error message
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'body.required' => 'The content field is required.',
+            'category_id.required' => 'The category field is required.',
+            'tag_id.required' => 'The tag field is required.',
+            'images.required' => 'The featured image field is required.',
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+
+            $this->merge(['user_id' => auth()->user()->id]);
+
+            if ($this['submit'] == 'draft') {
+
+                $this->merge(['published_at' => null]);
+            } 
+            else if ($this['submit'] == 'publish') {
+
+                $this->merge(['published_at' => Carbon::now()]);
+            }
+            else if ($this['submit'] == 'preview') {
+                
+                $this->merge(['previewed_at' => Carbon::now()]);
+            }
+            
+        });
     }
 }
