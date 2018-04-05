830ccbde33d7e283d0bc9f026e888eb2f925bfbe
R. Kukuh <rkukuh@gmail.com>
2018-04-05 22:10:23 +0700

    Create partner index view

---
+++ resources/views/admin/partner/index.blade.php
@@ -0,0 +1,110 @@
+@extends('admin.layouts.master')
+
+@section('content-title', 'PARTNER')
+@section('content-header', 'Partner List')
+
+@section('content-body')
+    <div style="margin-bottom: 60px;">
+        @component('common.buttons.create-new')
+            @slot('text', 'New Partner')
+            @slot('route', route('admin.partner.create'))
+        @endcomponent
+    </div>
+
+    @if ($partners->isEmpty())
+        @include('common.datalist.no-data')
+    @else
+        <table id="table1" class="table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th class="text-center">Partner Name</th>
+                    <th class="text-center">Creator</th>
+                    <th class="text-center">Created At</th>
+                    <th class="text-center">Published</th>
+                    <th style="width: 180px;"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($partners as $partner)
+                    <tr>
+                        <td class="text-center">
+                            @if ($partner->featured_image)
+                                <img src="{{ asset('storage/' . $partner->featured_image->path) }}" width="75px;">
+                            @else
+                                <div class="text-center">
+                                    <i class="fa fa-picture-o fa-5x text-muted"></i>
+                                </div>
+                            @endif
+                        </td>
+                        <td>
+                            <h5 style="font-size: 16px;">
+                                <a href="{{ route('admin.partner.show', $partner) }}">
+                                    {{ $partner->title }}
+                                </a>
+                            </h5>
+
+                            <small class="text-muted">
+                                Categories: {{ $partner->category_list }}
+                            </small> <br>
+
+                            <small class="text-muted">
+                                Tags: {{ $partner->tag_list }}
+                            </small>
+                        </td>
+                        <td>
+                            {{ $partner->author->name }} <br>
+
+                            <span class="fa fa-envelope-o"></span>
+                            <a href="mailto:{{ $partner->author->email }}">
+                                {{ $partner->author->email }}
+                            </a>
+                        </td>
+                        <td class="text-center">
+                            {!! $partner->created_at_formatted !!}
+                        </td>
+                        <td class="text-center">
+                            {!! $partner->published_at_formatted !!}
+                        </td>
+                        <td class="text-right">
+                            <form method="post" action="{{ route('admin.partner.update', $partner) }}" style="display: inline;">
+                                @method('PATCH')
+                                @csrf
+    
+                                @if ($partner->published_at)
+                                    @component('common.buttons.submit')
+                                        @slot('size', 'sm')
+                                        @slot('color', 'warning')
+                                        @slot('value', 'draft')
+                                        @slot('text', 'Set as Draft')
+                                    @endcomponent
+                                @else
+                                    @component('common.buttons.submit')
+                                        @slot('size', 'sm')
+                                        @slot('color', 'info')
+                                        @slot('value', 'publish')
+                                        @slot('text', 'Publish Now')
+                                    @endcomponent
+                                @endif
+                            </form>
+
+                            @component('common.datalist.button-edit')
+                                @slot('text', '')
+                                @slot('route', route('admin.partner.edit', $partner))
+                            @endcomponent
+
+                            @component('common.datalist.button-remove')
+                                @slot('text', '')
+                                @slot('route', route('admin.partner.destroy', $partner))
+                            @endcomponent
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="pull-right">
+            {{ $partners->links() }}
+        </div>
+    @endif
+@endsection
