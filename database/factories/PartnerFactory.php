e3dd06e7413285b1259f1bbb5d91fb8f98f158d1
R. Kukuh <rkukuh@gmail.com>
2018-04-05 15:54:28 +0700

    Define logic

--- database/factories/PartnerFactory.php
+++ database/factories/PartnerFactory.php
@@ -1,12 +1,23 @@
 <?php
 
+use App\User;
+use Carbon\Carbon;
 use App\Models\Partner;
 use Faker\Generator as Faker;
 
 $factory->define(Partner::class, function (Faker $faker) {
 
     return [
-        //
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->sentence,
+        'excerpt' => $faker->text,
+        'body' => $faker->text(3000),
+        'published_at' => $faker->randomElement([
+            null, 
+            Carbon::now()->addDay(rand(0, 3))
+        ])
     ];
 
 });
