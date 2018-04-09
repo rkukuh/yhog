31dbd6c2a83c581d667af0baa0d3ef965abae4e6
R. Kukuh <rkukuh@gmail.com>
2018-04-09 10:49:26 +0700

    Define

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,5 +9,20 @@ class Event extends Model
 {
     use SoftDeletes;
 
+    protected $fillable = [
+        'name',
+        'size',
+        'location',
+        'description',
+        
+        'price',
+        'early_bird_price',
+        'early_bird_price_end_at',
+
+        'start_at',
+        'end_at',
+        'total_hours',
+    ];
+
     protected $dates = ['deleted_at'];
 }
