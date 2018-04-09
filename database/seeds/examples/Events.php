dcf32cbb12ca1db8b0b49048ffcfe82f59fcca15
R. Kukuh <rkukuh@gmail.com>
2018-04-09 11:27:19 +0700

    Define logic for Event example data

--- database/seeds/examples/Events.php
+++ database/seeds/examples/Events.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\User;
+use Carbon\Carbon;
+use App\Models\Tag;
+use App\Models\Event;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class Events extends Seeder
 {
@@ -11,6 +17,35 @@ class Events extends Seeder
      */
     public function run()
     {
-        //
+        Collection::times(15, function ($number) {
+
+            /** Generate events data from its factory */
+
+            $custom_date = Carbon::now()->subDays(rand(3, 5));
+
+            $event = factory(Event::class)->create([
+                'user_id' => User::role(['admin'])->pluck('id')->random(),
+                'created_at' => $custom_date,
+                'updated_at' => $custom_date
+            ]);
+
+            /** Attach only one category (of event) to generated event data */
+
+            $event->categories()
+                     ->attach(
+                        Category::ofEvent()->pluck('id')->random()
+                    );
+
+            /** Attach tags to generated event data */
+
+            for ($i = 1; $i <= rand(2, 4); $i++) {
+
+                $event->tags()
+                     ->attach(
+                        Tag::pluck('id')->random()
+                     );
+            }
+
+        });
     }
 }
