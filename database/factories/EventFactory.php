55f9d1e45213d25d95210b44faaedc1b14945448
R. Kukuh <rkukuh@gmail.com>
2018-04-09 15:43:31 +0700

    Better logic

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -7,7 +7,21 @@ use Faker\Generator as Faker;
 
 $factory->define(Event::class, function (Faker $faker) {
 
+    $price = $faker->randomElement([null, 0, $faker->randomDigitNotNull * 1000000]);
+
     $start_at = Carbon::now()->addDay(rand(0, 3));
+
+    if ($price || ($price > 0)) {
+        $early_bird_price = 0.5 * $price;
+    } else {
+        $early_bird_price = $faker->randomElement([null, 0]);
+    }
+
+    if ($early_bird_price || ($early_bird_price > 0)) {
+        $early_bird_price_end_at = $start_at->subDay(1);
+    } else {
+        $early_bird_price_end_at = null;
+    }
 
     return [
         'user_id' => function() {
@@ -19,9 +33,9 @@ $factory->define(Event::class, function (Faker $faker) {
         'location' => $faker->randomElement([null, $faker->address]),
         'description' => $faker->randomElement([null, $faker->paragraph]),
         
-        'price' => $faker->randomElement([null, 0, $faker->randomDigitNotNull * 1000000]),
-        'early_bird_price' => $faker->randomElement([null, 0, $faker->randomDigitNotNull * 100000]),
-        'early_bird_price_end_at' => $faker->randomElement([null, $start_at]),
+        'price' => $price,
+        'early_bird_price' => $early_bird_price,
+        'early_bird_price_end_at' => $early_bird_price_end_at,
 
         'start_at' => $start_at,
         'end_at' => $faker->randomElement([null, Carbon::now()->addWeek(rand(2, 4))]),
