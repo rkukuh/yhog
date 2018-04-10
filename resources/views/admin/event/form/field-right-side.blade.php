685c33014feb2a000541eb34029f4ebad8ca43d7
R. Kukuh <rkukuh@gmail.com>
2018-04-10 16:40:36 +0700

    Better form

--- resources/views/admin/event/form/field-right-side.blade.php
+++ resources/views/admin/event/form/field-right-side.blade.php
@@ -1,5 +1,80 @@
 <div class="row">
     <div class="col-md-6">
+        <div class="form-group {{ $errors->has('start_at') ? 'has-error' : '' }}">
+            <label for="start_at">
+                Start Date @include('common.form.label-required-field')
+            </label>
+
+            <div class="input-group">
+                <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                </div>
+
+                <input type="text" class="form-control" id="start_at" name="start_at" 
+                        placeholder="dd/mm/yyyy"
+                        value="{{ old('start_at') ?
+                                    old('start_at') : (isset($event->start_at) ?
+                                                            $event->start_at->format('d/m/Y') :
+                                                            Carbon\Carbon::now()->format('d/m/Y')) }}">
+            </div>
+
+            @if ($errors->has('start_at'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('start_at')
+                ])
+            @endif
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group {{ $errors->has('end_at') ? 'has-error' : '' }}">
+            <label for="end_at">
+                End Date @include('common.form.label-required-field')
+            </label>
+
+            <div class="input-group">
+                <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                </div>
+
+                <input type="text" class="form-control" id="end_at" name="end_at" 
+                        placeholder="dd/mm/yyyy"
+                        value="{{ old('end_at') ?
+                                    old('end_at') : 
+                                        (isset($event->end_at) ?
+                                                $event->end_at->format('d/m/Y') :
+                                                Carbon\Carbon::now()->addDay(1)->format('d/m/Y')) }}">
+            </div>
+
+            @if ($errors->has('end_at'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('end_at')
+                ])
+            @endif
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group {{ $errors->has('total_hours') ? 'has-error' : '' }}">
+            <label for="total_hours">Total Hours</label>
+
+            <input type="text" class="form-control" id="total_hours" name="total_hours"
+                    value="{{ old('total_hours') ?: 
+                                (isset($event->total_hours) ? 
+                                        $event->total_hours : '') }}">
+
+            @if ($errors->has('total_hours'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('total_hours')
+                ])
+            @endif
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-6">
         <div class="form-group {{ $errors->has('price') ? 'has-error' : '' }}">
             <label for="price">Price</label>
 
@@ -70,81 +145,6 @@
             @if ($errors->has('early_bird_price_end_at'))
                 @include('common.form.input-error-message-no-feedback', [
                     'message' => $errors->first('early_bird_price_end_at')
-                ])
-            @endif
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-6">
-        <div class="form-group {{ $errors->has('start_at') ? 'has-error' : '' }}">
-            <label for="start_at">
-                Start Date @include('common.form.label-required-field')
-            </label>
-
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
-                </div>
-
-                <input type="text" class="form-control" id="start_at" name="start_at" 
-                        placeholder="dd/mm/yyyy"
-                        value="{{ old('start_at') ?
-                                    old('start_at') : (isset($event->start_at) ?
-                                                            $event->start_at->format('d/m/Y') :
-                                                            Carbon\Carbon::now()->format('d/m/Y')) }}">
-            </div>
-
-            @if ($errors->has('start_at'))
-                @include('common.form.input-error-message-no-feedback', [
-                    'message' => $errors->first('start_at')
-                ])
-            @endif
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="form-group {{ $errors->has('end_at') ? 'has-error' : '' }}">
-            <label for="end_at">
-                End Date @include('common.form.label-required-field')
-            </label>
-
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
-                </div>
-
-                <input type="text" class="form-control" id="end_at" name="end_at" 
-                        placeholder="dd/mm/yyyy"
-                        value="{{ old('end_at') ?
-                                    old('end_at') : 
-                                        (isset($event->end_at) ?
-                                                $event->end_at->format('d/m/Y') :
-                                                Carbon\Carbon::now()->addDay(1)->format('d/m/Y')) }}">
-            </div>
-
-            @if ($errors->has('end_at'))
-                @include('common.form.input-error-message-no-feedback', [
-                    'message' => $errors->first('end_at')
-                ])
-            @endif
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-6">
-        <div class="form-group {{ $errors->has('total_hours') ? 'has-error' : '' }}">
-            <label for="total_hours">Total Hours</label>
-
-            <input type="text" class="form-control" id="total_hours" name="total_hours"
-                    value="{{ old('total_hours') ?: 
-                                (isset($event->total_hours) ? 
-                                        $event->total_hours : '') }}">
-
-            @if ($errors->has('total_hours'))
-                @include('common.form.input-error-message-no-feedback', [
-                    'message' => $errors->first('total_hours')
                 ])
             @endif
         </div>
