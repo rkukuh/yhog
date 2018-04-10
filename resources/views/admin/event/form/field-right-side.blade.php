cba2c3d09f1ce856e58ad89bd3592e18f3117c21
R. Kukuh <rkukuh@gmail.com>
2018-04-10 16:39:01 +0700

    Spacing

--- resources/views/admin/event/form/field-right-side.blade.php
+++ resources/views/admin/event/form/field-right-side.blade.php
@@ -1,80 +1,80 @@
 <div class="row">
-        <div class="col-md-6">
-            <div class="form-group {{ $errors->has('price') ? 'has-error' : '' }}">
-                <label for="price">Price</label>
-    
-                <input type="number" class="form-control text-right" id="price" name="price"
-                        value="{{ old('price') ?: (isset($event->price) ? $event->price : 0) }}">
-    
-                @if ($errors->has('price'))
-                    @include('common.form.input-error-message-no-feedback', [
-                        'message' => $errors->first('price')
-                    ])
-                @endif
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group {{ $errors->has('size') ? 'has-error' : '' }}">
-                <label for="size">Participant Size</label>
-    
-                <input type="number" class="form-control text-right" id="size" name="size"
-                        value="{{ old('size') ?: (isset($event->size) ? $event->size : 0) }}">
-    
-                @if ($errors->has('size'))
-                    @include('common.form.input-error-message-no-feedback', [
-                        'message' => $errors->first('size')
-                    ])
-                @endif
-    
-                <span class="help-block">Leave 0 for no limitation</span>
-            </div>
+    <div class="col-md-6">
+        <div class="form-group {{ $errors->has('price') ? 'has-error' : '' }}">
+            <label for="price">Price</label>
+
+            <input type="number" class="form-control text-right" id="price" name="price"
+                    value="{{ old('price') ?: (isset($event->price) ? $event->price : 0) }}">
+
+            @if ($errors->has('price'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('price')
+                ])
+            @endif
         </div>
     </div>
-    
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group {{ $errors->has('early_bird_price') ? 'has-error' : '' }}">
-                <label for="early_bird_price">Early Bird Price</label>
-    
-                <input type="number" class="form-control text-right" id="early_bird_price" name="early_bird_price"
-                        value="{{ old('early_bird_price') ?: 
-                                    (isset($event->early_bird_price) ? 
-                                            $event->early_bird_price : 0) }}">
-    
-                @if ($errors->has('early_bird_price'))
-                    @include('common.form.input-error-message-no-feedback', [
-                        'message' => $errors->first('early_bird_price')
-                    ])
-                @endif
-    
-                <span class="help-block">Leave 0 for no early bird price</span>
-            </div>
+    <div class="col-md-6">
+        <div class="form-group {{ $errors->has('size') ? 'has-error' : '' }}">
+            <label for="size">Participant Size</label>
+
+            <input type="number" class="form-control text-right" id="size" name="size"
+                    value="{{ old('size') ?: (isset($event->size) ? $event->size : 0) }}">
+
+            @if ($errors->has('size'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('size')
+                ])
+            @endif
+
+            <span class="help-block">Leave 0 for no limitation</span>
         </div>
-        <div class="col-md-6">
-            <div class="form-group {{ $errors->has('early_bird_price_end_at') ? 'has-error' : '' }}">
-                <label for="early_bird_price_end_at">Early Bird End Date</label>
+    </div>
+</div>
     
-                <div class="input-group">
-                    <div class="input-group-addon">
-                        <i class="fa fa-calendar"></i>
-                    </div>
-    
-                    <input type="text" class="form-control" id="early_bird_price_end_at" name="early_bird_price_end_at" 
-                            placeholder="dd/mm/yyyy"
-                            value="{{ old('early_bird_price_end_at') ?
-                                        old('early_bird_price_end_at') : 
-                                            (isset($event->early_bird_price_end_at) ?
-                                                    $event->early_bird_price_end_at->format('d/m/Y') : '') }}">
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group {{ $errors->has('early_bird_price') ? 'has-error' : '' }}">
+            <label for="early_bird_price">Early Bird Price</label>
+
+            <input type="number" class="form-control text-right" id="early_bird_price" name="early_bird_price"
+                    value="{{ old('early_bird_price') ?: 
+                                (isset($event->early_bird_price) ? 
+                                        $event->early_bird_price : 0) }}">
+
+            @if ($errors->has('early_bird_price'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('early_bird_price')
+                ])
+            @endif
+
+            <span class="help-block">Leave 0 for no early bird price</span>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group {{ $errors->has('early_bird_price_end_at') ? 'has-error' : '' }}">
+            <label for="early_bird_price_end_at">Early Bird End Date</label>
+
+            <div class="input-group">
+                <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
                 </div>
-    
-                @if ($errors->has('early_bird_price_end_at'))
-                    @include('common.form.input-error-message-no-feedback', [
-                        'message' => $errors->first('early_bird_price_end_at')
-                    ])
-                @endif
+
+                <input type="text" class="form-control" id="early_bird_price_end_at" name="early_bird_price_end_at" 
+                        placeholder="dd/mm/yyyy"
+                        value="{{ old('early_bird_price_end_at') ?
+                                    old('early_bird_price_end_at') : 
+                                        (isset($event->early_bird_price_end_at) ?
+                                                $event->early_bird_price_end_at->format('d/m/Y') : '') }}">
             </div>
+
+            @if ($errors->has('early_bird_price_end_at'))
+                @include('common.form.input-error-message-no-feedback', [
+                    'message' => $errors->first('early_bird_price_end_at')
+                ])
+            @endif
         </div>
     </div>
+</div>
 
 <div class="row">
     <div class="col-md-6">
