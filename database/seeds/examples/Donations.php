3fd69f7ebc85b0061adf20e5e930dabaed07996a
R. Kukuh <rkukuh@gmail.com>
2018-04-11 16:48:44 +0700

    Define logic for Donation example data

--- database/seeds/examples/Donations.php
+++ database/seeds/examples/Donations.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\User;
+use Carbon\Carbon;
+use App\Models\Tag;
+use App\Models\Category;
+use App\Models\Donation;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class Donations extends Seeder
 {
@@ -11,6 +17,35 @@ class Donations extends Seeder
      */
     public function run()
     {
-        //
+        Collection::times(25, function ($number) {
+
+            /** Generate donations data from its factory */
+
+            $custom_date = Carbon::now()->subDays(rand(3, 5));
+
+            $donation = factory(Donation::class)->create([
+                'user_id' => User::role(['admin'])->pluck('id')->random(),
+                'created_at' => $custom_date,
+                'updated_at' => $custom_date
+            ]);
+
+            /** Attach only one category to generated donation data */
+
+            $donation->categories()
+                     ->attach(
+                        Category::ofdonation()->pluck('id')->random()
+                    );
+
+            /** Attach tags to generated donation data */
+
+            for ($i = 1; $i <= rand(2, 4); $i++) {
+
+                $donation->tags()
+                     ->attach(
+                        Tag::pluck('id')->random()
+                     );
+            }
+
+        });
     }
 }
