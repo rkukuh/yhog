a241f7eecce000192d4547c4cba9f2b0e7e12ce4
R. Kukuh <rkukuh@gmail.com>
2018-04-11 15:44:13 +0700

    Define logic for Donation factory

--- database/factories/DonationFactory.php
+++ database/factories/DonationFactory.php
@@ -6,7 +6,18 @@ use Faker\Generator as Faker;
 $factory->define(Donation::class, function (Faker $faker) {
 
     return [
-        //
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+
+        'title' => $faker->sentence,
+        'target' => $faker->randomElement([null, 0, $faker->randomDigitNotNull * 1000000]),
+        'location' => $faker->randomElement([null, $faker->address]),
+        'end_at' => $faker->randomElement([null, Carbon::now()->addDay(rand(0, 3))]),
+
+        'video_url' => 'www.myvideo.example/' . $faker->uuid,
+        'excerpt' => $faker->text,
+        'description' => $faker->text(3000),
     ];
 
 });
