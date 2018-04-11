ed9fe1b079b38b48ab5bc280ee266cdb959094d2
R. Kukuh <rkukuh@gmail.com>
2018-04-11 19:35:11 +0700

    FIX: Missing classes import

--- app/Http/Controllers/Admin/DonationController.php
+++ app/Http/Controllers/Admin/DonationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Tag;
+use App\Models\Category;
 use App\Models\Donation;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\DonationStore;
