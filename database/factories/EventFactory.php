66c45e7fc2771335a0776362fccc54f31165030a
R. Kukuh <rkukuh@gmail.com>
2018-04-12 11:51:34 +0700

    FIX: Weird behaviour of Carbon; Use parsing first instead of direct use

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -9,7 +9,7 @@ $factory->define(Event::class, function (Faker $faker) {
 
     $price = $faker->randomElement([null, 0, $faker->randomDigitNotNull * 1000000]);
 
-    $start_at = Carbon::now()->addDay(rand(0, 3));
+    $start_at = Carbon::now()->addDay(rand(-1, 2));
 
     if ($price || ($price > 0)) {
         $early_bird_price = 0.5 * $price;
@@ -18,7 +18,7 @@ $factory->define(Event::class, function (Faker $faker) {
     }
 
     if ($early_bird_price || ($early_bird_price > 0)) {
-        $early_bird_price_end_at = $start_at->subDay(1);
+        $early_bird_price_end_at = Carbon::parse($start_at)->subDay(1);
     } else {
         $early_bird_price_end_at = null;
     }
@@ -37,8 +37,8 @@ $factory->define(Event::class, function (Faker $faker) {
         'early_bird_price' => $early_bird_price,
         'early_bird_price_end_at' => $early_bird_price_end_at,
 
-        'start_at' => $start_at,
-        'end_at' => $faker->randomElement([null, Carbon::now()->addWeek(rand(2, 4))]),
+        'start_at' => new Carbon($start_at), 
+        'end_at' => $faker->randomElement([null, Carbon::parse($start_at)->addWeek(rand(0, 4))]),
     ];
 
 });
