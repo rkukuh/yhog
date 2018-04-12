8b6a54df005740e9a7f2527dbf9f9655120c1327
R. Kukuh <rkukuh@gmail.com>
2018-04-12 16:17:25 +0700

    Use trait instead

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use App\User;
-use Carbon\Carbon;
 use App\Traits\Taggable;
 use App\Traits\Imageable;
+use App\Traits\HasSchedule;
 use App\Traits\Categorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,6 +15,7 @@ class Donation extends Model
     use Taggable;
     use Imageable;
     use SoftDeletes;
+    use HasSchedule;
     use Categorizable;
 
     protected $fillable = [
@@ -83,132 +84,11 @@ class Donation extends Model
         }
     }
 
-    public function getStartAtFormattedAttribute()
-    {
-        if ($this->hasEnded()) {
-
-            echo '<span class="label label-warning">ENDED</span> <br>' .
-                    '<span class="text-muted">' .
-                        $this->start_at->format('d-M-Y') .
-                    '</span>';
-
-            return;
-        }
-
-        $output = '';
-
-        if ($this->isWaiting()) {
-
-            $output = '<span class="label label-default">WAITING</span> <br>';
-            $output .= '<small>' . $this->start_at->diffForHumans() . '</small>';
-        }
-
-        if ($this->isActive()) {
-
-            $output = '<span class="label label-success">ACTIVE</span>';
-        }
-
-        $output .= '<br>' .
-                    '<span class="text-muted">' .
-                        $this->start_at->format('d-M-Y') .
-                    '</span>';
-
-        echo $output;
-    }
-
-    public function getEndAtFormattedAttribute()
-    {
-        if ($this->isEndless()) {
-
-            echo '<span class="label label-info">ENDLESS</span> <br> ' .
-                    '<small class="text-muted">until manually ended</small>';
-
-            return;
-        }
-
-        if ($this->hasEnded()) {
-
-            $output = '<span class="label label-warning">ENDED</span>';
-        }
-        else {
-
-            $output = '<span class="label label-default">WAITING</span> <br>';
-            $output .= '<small>' . $this->end_at->diffForHumans() . '</small>';
-        }
-
-        $output .=  '<br>' .
-                    '<span class="text-muted">' .
-                        $this->end_at->format('d-M-Y') .
-                    '</span>';
-
-        echo $output;
-    }
-
     public function getCreatedAtFormattedAttribute()
     {
         echo $this->created_at->diffForHumans() . '<br>' .
                 '<small class="text-muted">' .
                     $this->created_at->format('d-M-Y') .
                 '</small>';
-    }
-
-
-    /***************************************** HELPER ********************************************/
-
-    /**
-     * Determine whether an event is active
-     *
-     * @return boolean
-     */
-    public function isActive()
-    {
-        if ( ! $this->isWaiting() ) return true;
-
-        return false;
-    }
-
-    /**
-     * Determine whether an event is waiting for active period
-     *
-     * @return boolean
-     */
-    public function isWaiting()
-    {
-        $now = Carbon::now();
-
-        if ($now->diffInDays($this->start_at, false) > 0) {
-
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Determine whether an event is already ended
-     *
-     * @return boolean
-     */
-    public function hasEnded()
-    {
-        if ($this->end_at != null
-            && Carbon::now() > $this->end_at) {
-
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Determine whether an event has no expire date
-     *
-     * @return boolean
-     */
-    public function isEndless()
-    {
-        if ($this->end_at == null) return true;
-
-        return false;
     }
 }
