01f3e4cafb0c1923bb5af69ee2e585a9558883dd
R. Kukuh <rkukuh@gmail.com>
2018-04-15 19:13:56 +0700

    Define 1-M relationship between Gallery and User

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -16,4 +16,20 @@ class Gallery extends Model
     use Categorizable;
 
     protected $dates = ['deleted_at'];
+
+
+    /*************************************** RELATIONSHIP ****************************************/
+
+    /**
+     * One-to-Many: A creator may create zero or many gallery.
+     *
+     * This function will retrieve the creator of a gallery.
+     * See: User' galleries() method for the inverse
+     *
+     * @return mixed
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
