11892a82b441fb843ae30b2abe83f3ee2a3a096b
R. Kukuh <rkukuh@gmail.com>
2018-04-15 19:11:14 +0700

    Use taggable, categorizable, and imageable traits

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use App\Traits\Taggable;
+use App\Traits\Imageable;
+use App\Traits\Categorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Gallery extends Model
 {
+    use Taggable;
+    use Imageable;
     use SoftDeletes;
+    use Categorizable;
 
     protected $dates = ['deleted_at'];
 }
