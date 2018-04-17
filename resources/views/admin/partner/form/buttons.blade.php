62fd45a333f291a2f840bac35bd49717cdfe2c28
R. Kukuh <rkukuh@gmail.com>
2018-04-17 10:07:52 +0700

    No 'preview' action for Partner

--- resources/views/admin/partner/form/buttons.blade.php
+++ resources/views/admin/partner/form/buttons.blade.php
@@ -1,13 +1,5 @@
 @component('common.buttons.submit')
     @slot('size', 'lg')
-    @slot('text', 'Preview')
-    @slot('target', '_blank')
-    @slot('class', 'btn-block')
-    @slot('value', 'preview')
-@endcomponent
-
-@component('common.buttons.submit')
-    @slot('size', 'lg')
     @slot('color', 'warning')
     @slot('text', 'Save Draft')
     @slot('class', 'btn-block')
