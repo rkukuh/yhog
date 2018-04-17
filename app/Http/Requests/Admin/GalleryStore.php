e9c2a0119f3d4ae1153cbeba50a3ad46090765ca
R. Kukuh <rkukuh@gmail.com>
2018-04-17 10:39:58 +0700

    Define authorize(), rules(), messages(), and validation hook for Gallery's store request

--- app/Http/Requests/Admin/GalleryStore.php
+++ app/Http/Requests/Admin/GalleryStore.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Admin;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
 class GalleryStore extends FormRequest
@@ -13,7 +14,7 @@ class GalleryStore extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return auth()->user()->hasRole('admin');
     }
 
     /**
@@ -24,7 +25,50 @@ class GalleryStore extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|min:3|max:100',
+            'category_id' => 'required|exists:categories,id',
+            'description' => 'required|min:5',
+            'images' => 'required',
+            'images.*.image' => 'mimes:jpeg,png|min:50|max:2000',
+            'tag_id' => 'nullable|exists:tags,id',
         ];
+    }
+
+    /**
+     * Set custom validation error message
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'category_id.required' => 'The category field is required.',
+            'images.required' => 'The images field is required.',
+            'tag_id.required' => 'The tag field is required.',
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+
+            $this->merge(['user_id' => auth()->user()->id]);
+
+            if ($this['submit'] == 'draft') {
+
+                $this->merge(['published_at' => null]);
+            } 
+            else if ($this['submit'] == 'publish') {
+
+                $this->merge(['published_at' => Carbon::now()]);
+            }
+            
+        });
     }
 }
