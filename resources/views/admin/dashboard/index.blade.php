e5681d10977255e86e44b0346a1599eb2ad0ce7f
R. Kukuh <rkukuh@gmail.com>
2018-04-17 12:59:11 +0700

    Remove content

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,13 +1,3 @@
 @extends('admin.layouts.master')
 
-@section('content-title')
-    DASHBOARD
-    <small>Small heading</small>
-@endsection
-
-@section('content-header', 'Content header goes here')
-@section('content-footer', 'Content footer goes here')
-
-@section('content-body')
-    Main content goes here
-@endsection
+@section('content-title', 'YHOG DASHBOARD')
