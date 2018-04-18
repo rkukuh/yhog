d048a45cf38f2d946c5d3a786d2e5d0be7f4b04f
R. Kukuh <rkukuh@gmail.com>
2018-04-18 18:29:21 +0700

    Create donation show view

---
+++ resources/views/admin/donation/show.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.layouts.master')
+
+@section('content-title')
+    <small class="text-muted">Donation Detail</small>
+    <h3>{{ $donation->title }}</h3>
+@endsection
+
+@section('content-header')
+@endsection
+
+@section('content-body')
+    <div class="row">
+        <div class="col-md-5">
+            <h4>Donation Info</h4>
+
+            <div class="list-group">
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Target</h4>
+
+                    <p class="list-group-item-text">
+                        {{ $donation->target_formatted }}
+                    </p>
+                </a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Deadline</h4>
+
+                    <p class="list-group-item-text">
+                        {{ $donation->deadline }}
+                    </p>
+                </a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Description</h4>
+
+                    <p class="list-group-item-text">
+                        {{ $donation->description }}
+                    </p>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <h4>Receive History</h4>
+
+            <ul class="list-group">
+                @foreach ($donation->donates as $donate)
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">
+                                {{ $donate->amount_formatted }}
+                            </div>
+                            <div class="col-md-4 pull-right">
+                                {{ $donate->created_at_formatted }}
+                            </div>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endsection
