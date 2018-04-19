8af66be4ea95a1ab077bba2f75b507778d0fdb24
R. Kukuh <rkukuh@gmail.com>
2018-04-19 14:22:43 +0700

    Define custom route

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,8 @@ Route::name('admin.')->group(function () {
         /** DONATION **/
 
         Route::resource('donation', 'DonationController');
+
+        Route::post('/donate/{donation}', 'DonateController@store')->name('donate.store');
         Route::resource('donate', 'DonateController');
     
         Route::resource('category-donation', 'CategoryDonationController', [
