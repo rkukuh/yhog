f4699e68df62bd8df1ea115741d7473b7b7e6f26
R. Kukuh <rkukuh@gmail.com>
2018-04-23 21:36:30 +0700

    Remove unused css rules

--- resources/views/admin/partner/_form.blade.php
+++ resources/views/admin/partner/_form.blade.php
@@ -12,17 +12,6 @@
     <link rel="stylesheet" href="{{ asset('assets/adminlte/bower_components/select2/dist/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/adminlte/plugins/iCheck/all.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/summernote/summernote.css') }}">
-
-    <style>
-        fieldset h4 {
-            border-bottom: 1px solid #ddd;
-            margin: 20px 0;
-            padding-bottom: 5px;
-        }
-
-        .radio > label:first-child { margin-left: -40px; }
-        /* .radio > label { margin-right: 20px; } */
-    </style>
 @endpush
 
 @push('footer-scripts')
