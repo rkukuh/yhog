e93d4a269d09b80a28f8d47e45524830b4cc12c2
R. Kukuh <rkukuh@gmail.com>
2018-04-25 00:26:13 +0700

    Define traits, ,

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\Imageable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Advertisement extends Model
 {
-    //
+    use Imageable;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'partner_id',
+        'url',
+    ];
+
+    protected $dates = ['deleted_at'];
 }
