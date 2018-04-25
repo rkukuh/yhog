5184a2a39679e27532e8a03215d65fb7bda5f5ed
R. Kukuh <rkukuh@gmail.com>
2018-04-25 15:16:15 +0700

    Construct datalist for Ads

--- database/seeds/examples/Advertisements.php
+++ database/seeds/examples/Advertisements.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use App\Models\Partner;
 use App\Models\Advertisement;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,17 @@ class Advertisements extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= Partner::count(); $i++) {
+        for ($i = 21; $i <= Partner::count(); $i++) {
 
             factory(Advertisement::class)->create([
                 'partner_id' => $i
             ]);
         }
+
+        // Deactivate all Ad Unit
+        Advertisement::query()->update(['activated_at' => null]);
+
+        // Activate only one Ad Unit, randomly
+        Advertisement::get()->random()->update(['activated_at' => Carbon::now()]);
     }
 }
---
+++ resources/views/admin/advertisement/index.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.layouts.master')
+
+@section('content-title', 'ADVERTISEMENT')
+@section('content-header', 'Advertisement List')
+
+@section('content-body')
+    @if ($advertisements->isEmpty())
+        @include('common.datalist.no-data')
+    @else
+        <table id="table1" class="table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th class="text-center">Partner</th>
+                    <th class="text-center">URL Target</th>
+                    <th class="text-center">Activated</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($advertisements as $advertisement)
+                    <tr>
+                        <td class="text-center">
+                            @if ($advertisement->banner)
+                                <img src="{{ asset('storage/' . $advertisement->banner->path) }}" width="75px;">
+                            @else
+                                <div class="text-center">
+                                    <i class="fa fa-picture-o fa-5x text-muted"></i>
+                                </div>
+                            @endif
+                        </td>
+                        <td>
+                            {{ $advertisement->partner->title }}
+                        </td>
+                        <td>
+                            <a href="{{ $advertisement->url }}" target="_blank">
+                                {{ $advertisement->url }}
+                            </a>
+                        </td>
+                        <td class="text-center">
+                            {{ $advertisement->activated_at_formatted }}
+                        </td>
+                        <td class="text-right">
+                            <form method="post" action="{{ route('admin.partner.update', $advertisement) }}" style="display: inline;">
+                                @csrf
+                                @method('PATCH')
+    
+                                @if ($advertisement->activated_at)
+                                    @component('common.buttons.submit')
+                                        @slot('size', 'sm')
+                                        @slot('color', 'warning')
+                                        @slot('value', 'deactivate')
+                                        @slot('text', 'Deactivate')
+                                    @endcomponent
+                                @else
+                                    @component('common.buttons.submit')
+                                        @slot('size', 'sm')
+                                        @slot('color', 'info')
+                                        @slot('value', 'activate')
+                                        @slot('text', 'Activate')
+                                    @endcomponent
+                                @endif
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+@endsection
