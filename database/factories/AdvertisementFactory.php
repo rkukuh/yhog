c146bd4aa0b60b18f9cecee24adf8bf5a283e09a
R. Kukuh <rkukuh@gmail.com>
2018-04-25 14:41:47 +0700

    Define logic for factory

--- database/factories/AdvertisementFactory.php
+++ database/factories/AdvertisementFactory.php
@@ -1,12 +1,18 @@
 <?php
 
+use Carbon\Carbon;
+use App\Models\Partner;
 use App\Models\Advertisement;
 use Faker\Generator as Faker;
 
 $factory->define(Advertisement::class, function (Faker $faker) {
 
     return [
-        //
+        'partner_id' => function() {
+            return factory(Partner::class)->create()->id;
+        },
+        'url' => $faker->url,
+        'activated_at' => $faker->randomElement([null, Carbon::now()])
     ];
 
 });
