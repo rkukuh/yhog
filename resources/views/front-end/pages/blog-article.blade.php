cf2530f23a24e6957cff9a2fe9d09f751c6765ee
R. Kukuh <rkukuh@gmail.com>
2018-04-26 16:07:04 +0700

    Enumerate post attribute

--- resources/views/front-end/pages/blog-article.blade.php
+++ resources/views/front-end/pages/blog-article.blade.php
@@ -1,123 +1,107 @@
 @extends('front-end.templates._base')
 
 @push('page-meta-tags')
-<title>Blog Article - Yayasan HOG</title>
+	<title>Blog Article - Yayasan HOG</title>
 @endpush
 
 @push('body-class')
-<body id="article-page">
+	<body id="article-page">
 @endpush
 
 @section('content')
-<section class="featured">
-	<div class="grid-container">
-		<div class="grid-x grid-padding-x">
-			<div class="cell xsmall-12 large-6">
-				<img src="{{ asset('assets/img/event-sample-large.jpg') }}">
-			</div>
-			
-			<div class="cell xsmall-12 large-6">
-				<h2><span>Yayasan Harley Owners Group</span><br><span>2018 Charity Golf Scramble</span></h2>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="detail">
-	<div class="grid-container">
-		<div class="grid-x grid-padding-x">
-			<div class="cell xsmall-12 large-9">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				
-				<p>fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-			
-			<aside class="cell xsmall-12 large-3">
-				<img src="{{ asset('assets/img/logo-sponsor.png') }}">
-			</aside>
-		</div>
-	</div>
-</section>
-
-<section class="article-list">
-	<div class="grid-container">
-		<div class="grid-x grid-margin-x">
-			<div class="cell">
-				<h3>Related articles</h3>
-			</div>
-		</div>
-		
-		<div class="items grid-x grid-margin-x xsmall-up-1 medium-up-2 large-up-4">
-			<div class="item cell">
-				<img src="{{ asset('assets/img/event-sample-large.jpg') }}">
-				
-				<h3>Yayasan Harley Owners Group 2018 Charity Golf Scramble</h3>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam&hellip;</p>
-				
-				<div class="text-center"><a class="cta" href="{{ url('blog/article') }}">Full Details</a></div>
-			</div>
-			
-			<div class="item cell">
-				<img src="{{ asset('assets/img/event-sample-large.jpg') }}">
-				
-				<h3>Yayasan Harley Owners Group 2018 Charity Golf Scramble</h3>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam&hellip;</p>
-				
-				<div class="text-center"><a class="cta" href="{{ url('blog/article') }}">Full Details</a></div>
-			</div>
-			
-			<div class="item cell">
-				<img src="{{ asset('assets/img/event-sample-large.jpg') }}">
-				
-				<h3>Yayasan Harley Owners Group 2018 Charity Golf Scramble</h3>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam&hellip;</p>
-				
-				<div class="text-center"><a class="cta" href="{{ url('blog/article') }}">Full Details</a></div>
-			</div>
-			
-			<div class="item cell">
-				<img src="{{ asset('assets/img/event-sample-large.jpg') }}">
-				
-				<h3>Yayasan Harley Owners Group 2018 Charity Golf Scramble</h3>
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam&hellip;</p>
-				
-				<div class="text-center"><a class="cta" href="{{ url('blog/article') }}">Full Details</a></div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="support-us">
-	<header>
+	<section class="featured">
 		<div class="grid-container">
-			<div class="grid-x grid-padding-x align-center">
-				<div class="cell xsmall-12 large-10 text-center">
-					<p>Every ticket purchased goes a long way in helping us achieve our fundraising goals.</p>
+			<div class="grid-x grid-padding-x">
+				<div class="cell xsmall-12 large-6">
+					@if ($post->featured_image)
+						<img src="{{ asset('storage/' . $post->featured_image->path) }}">
+					@endif
+				</div>
+				
+				<div class="cell xsmall-12 large-6">
+					<h2>
+						<span>{{ $post->title }}</span>
+					</h2>
+					
+					<p>{{ $post->excerpt }}</p>
 				</div>
 			</div>
 		</div>
-	</header>
-	
-	<div class="grid-container">
-		@include('front-end.common.elements.goals')
-	
-		<div class="grid-x grid-padding-x">
-			<div class="cell xsmall-12 text-center">
-				<h2>Help us, help more kids.</h2>
+	</section>
+
+	<section class="detail">
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x">
+				<div class="cell xsmall-12 large-9">
+					<p>{{ $post->body }}</p>
+				</div>
 				
-				<a class="cta">Support Us Today!</a>
+				<aside class="cell xsmall-12 large-3">
+					<img src="{{ asset('assets/img/logo-sponsor.png') }}">
+				</aside>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+
+	@if (isset($more_posts))
+		<section class="article-list">
+			<div class="grid-container">
+				<div class="grid-x grid-margin-x">
+					<div class="cell">
+						<h3>Related articles</h3>
+					</div>
+				</div>
+				
+				@foreach ($more_posts->chunk(4) as $chunked_more_posts)
+					<div class="items grid-x grid-margin-x xsmall-up-1 medium-up-2 large-up-4">
+
+						@foreach ($chunked_more_posts as $post)
+							<div class="item cell">
+								@if ($post->featured_image)
+									<img src="{{ asset('storage/' . $post->featured_image->path) }}">
+								@endif
+								
+								<h3>{{ $post->title }}</h3>
+								
+								<p>{{ $post->excerpt }}&hellip;</p>
+								
+								<div class="text-center">
+									<a class="cta" href="{{ url('blog/article/' . $post->id) }}">
+										Full Details
+									</a>
+								</div>
+							</div>
+						@endforeach
+
+					</div>
+				@endforeach
+			</div>
+		</section>
+	@endif
+
+	<section class="support-us">
+		<header>
+			<div class="grid-container">
+				<div class="grid-x grid-padding-x align-center">
+					<div class="cell xsmall-12 large-10 text-center">
+						<p>Every ticket purchased goes a long way in helping us achieve our fundraising goals.</p>
+					</div>
+				</div>
+			</div>
+		</header>
+		
+		<div class="grid-container">
+			@include('front-end.common.elements.goals')
+		
+			<div class="grid-x grid-padding-x">
+				<div class="cell xsmall-12 text-center">
+					<h2>Help us, help more kids.</h2>
+					
+					<a class="cta">Support Us Today!</a>
+				</div>
+			</div>
+		</div>
+	</section>
 @endsection
 
 @push('page-styles')
