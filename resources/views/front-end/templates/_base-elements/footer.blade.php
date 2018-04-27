11574823fe6522dc2917e80a42889f32a4153233
firdaus riyanto <firdaus@madewow.com>
2018-04-27 14:10:03 +0700

    footer

--- resources/views/front-end/templates/_base-elements/footer.blade.php
+++ resources/views/front-end/templates/_base-elements/footer.blade.php
@@ -1,2 +1,69 @@
 <footer>
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell xsmall-12 medium-4 large-3">
+				<ul class="footer-navigation">
+					<li>
+						<a href="{{ url('') }}">Home</a>
+					</li>
+					
+					<li>
+						<a href="{{ url('/our-projects') }}">Our Projects</a>
+					</li>
+					
+					<li>
+						<a href="{{ url('/events') }}">Events</a>
+					</li>
+					
+					<li>
+						<a href="{{ url('/about-us') }}">About Us</a>
+					</li>
+				</ul>
+			</div>
+			
+			<div class="cell xsmall-12 medium-4 large-3">
+				<ul class="footer-navigation">
+					<li>
+						<a href="{{ url('/partners') }}">Partners</a>
+					</li>
+					
+					<li>
+						<a href="{{ url('/blog') }}">Blog</a>
+					</li>
+					
+					<li>
+						<a href="{{ url('/gallery') }}">Gallery</a>
+					</li>
+					
+					<li>
+						<a href="{{ url('/contact-us') }}">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+			
+			<div class="cell xsmall-12 medium-4 large-3">
+				<h3>Help us, help more kids.</h3>
+				
+				<a class="cta">Support Us Today</a>
+			</div>
+			
+			<div class="cell xsmall-12 medium-12 large-3 text-center">
+				<h3 class="follow">Follow us</h3>
+				
+				<ul class="follow-navigation">
+					<li>
+						<a href="https://www.facebook.com/groups/65514237783">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+					
+					<li>
+						<a href="https://www.instagram.com/yayasanyhogs/">
+							<i class="fab fa-instagram"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </footer>
