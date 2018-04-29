c55c9201d8d9a9cf9d1d6a179af481600756e684
R. Kukuh <rkukuh@gmail.com>
2018-04-29 14:53:37 +0700

    Add validation rule for partner field

--- app/Http/Requests/Admin/EventStore.php
+++ app/Http/Requests/Admin/EventStore.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Admin;
 
 use Carbon\Carbon;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class EventStore extends FormRequest
@@ -31,13 +32,14 @@ class EventStore extends FormRequest
             'description' => 'nullable|min:5',
             'images' => 'nullable',
             'images.*.image' => 'mimes:jpeg,png|min:50|max:1000',
-            
+
             'start_at' => 'required|date_format:"d/m/Y"|after_or_equal:today',
             'end_at' => 'required|date_format:"d/m/Y"|after_or_equal:start_at',
             'price' => 'numeric|min:0',
             'size' => 'numeric|min:0',
             'early_bird_price' => 'numeric|min:0',
             'early_bird_price_end_at' => 'nullable|date_format:"d/m/Y"|before_or_equal:start_at',
+            'partner_id' => 'nullable|exists:partners,id',
             'tag_id' => 'nullable|exists:tags,id',
         ];
     }
