6e7216e8ecca5bdabaefc5986e66efcbfe6251c6
R. Kukuh <rkukuh@gmail.com>
2018-05-02 20:11:01 +0700

    Create controller to handle donation from frontend

---
+++ app/Http/Controllers/DonateController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Donate;
+use App\Http\Requests\DonateStore;
+use App\Http\Requests\DonateUpdate;
+
+class DonateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \DonateStore  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DonateStore $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Donate  $donate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Donate $donate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Donate  $donate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Donate $donate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \DonateUpdate  $request
+     * @param  \App\Models\Donate  $donate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DonateUpdate $request, Donate $donate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Donate  $donate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Donate $donate)
+    {
+        //
+    }
+}
