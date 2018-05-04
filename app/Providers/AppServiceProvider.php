fd7c8fa1ce38521e0ddf4cc2870e985301dd807f
R. Kukuh <rkukuh@gmail.com>
2018-05-04 19:51:43 +0700

    Mapping a morphed relationship

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Relation::morphMap([
+            'post' => 'App\Models\Post',
+            'event' => 'App\Models\Event',
+            'gallery' => 'App\Models\Gallery',
+            'partner' => 'App\Models\Partner',
+            'donation' => 'App\Models\Donation',
+            'advertisement' => 'App\Models\Advertisement',
+        ]);
     }
 
     /**
