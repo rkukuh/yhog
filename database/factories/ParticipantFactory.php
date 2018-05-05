a1b6e7cecbf55fcb324bba7d990ad7983756fa6f
R. Kukuh <rkukuh@gmail.com>
2018-05-05 20:09:29 +0700

    Define logic to fakering participant factory

--- database/factories/ParticipantFactory.php
+++ database/factories/ParticipantFactory.php
@@ -1,12 +1,26 @@
 <?php
 
+use App\User;
+use App\Models\Event;
 use App\Models\Participant;
 use Faker\Generator as Faker;
 
 $factory->define(Participant::class, function (Faker $faker) {
 
     return [
-        //
+        'creator_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'event_id' => function() {
+            return factory(Event::class)->create()->id;
+        },
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'quantity' => $faker->randomDigitNotNull,
+        'price' => $faker->randomElement([0, $faker->randomDigitNotNull * 100000]),
+        'response' => null,
     ];
 
 });
