e876d27545245bdb9b424f7f1dde62c089eaa5dd
R. Kukuh <rkukuh@gmail.com>
2018-05-05 20:36:08 +0700

    Populate participant example data

--- database/seeds/examples/Participants.php
+++ database/seeds/examples/Participants.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\User;
+use App\Models\Event;
+use App\Models\Participant;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class Participants extends Seeder
 {
@@ -11,6 +15,23 @@ class Participants extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 11; $i <= Event::count(); $i++) {
+
+            Collection::times(rand(1, 3), function ($times) use ($i) {
+
+                $event = Event::get()->random();
+
+                Event::find($i)
+                        ->participants()
+                        ->save(
+                            factory(Participant::class)->create([
+                                'creator_id' => User::role(['admin'])->pluck('id')->random(),
+                                'event_id' => $event->id,
+                                'price' => $event->price,
+                            ])
+                        );
+
+            });
+        }
     }
 }
