4f5978de13d6ad78910890fda8f34b8196d6d0aa
R. Kukuh <rkukuh@gmail.com>
2018-05-06 22:37:11 +0700

    Disable test route for security reason

--- routes/web.php
+++ routes/web.php
@@ -106,29 +106,29 @@ Route::resource('/participant', 'ParticipantController');
 
 ////////////////////////////// TESTING //////////////////////////////
 
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client as GuzzleHttpClient;
+// use GuzzleHttp\Exception\GuzzleException;
+// use GuzzleHttp\Client as GuzzleHttpClient;
 
-Route::get('/test/convert/{amount}', function ($amount) {
+// Route::get('/test/convert/{amount}', function ($amount) {
 
-    $client = new GuzzleHttpClient;
+//     $client = new GuzzleHttpClient;
 
-    $response = $client->get('http://free.currencyconverterapi.com/api/v5/convert', [
-        'query' => [
-            'q' => 'USD_IDR',
-            'compact' => 'ultra',
-        ]
-    ])
-    ->getBody()
-    ->getContents();
+//     $response = $client->get('http://free.currencyconverterapi.com/api/v5/convert', [
+//         'query' => [
+//             'q' => 'USD_IDR',
+//             'compact' => 'ultra',
+//         ]
+//     ])
+//     ->getBody()
+//     ->getContents();
 
-    $decoded_result = json_decode($response, true);
+//     $decoded_result = json_decode($response, true);
 
-    $exchange_rate  = $decoded_result['USD_IDR'];
-    $converted      = $exchange_rate * $amount;
+//     $exchange_rate  = $decoded_result['USD_IDR'];
+//     $converted      = $exchange_rate * $amount;
 
-    echo '1 USD = ' . number_format($exchange_rate) . ' IDR';
-    echo '<br>';
-    echo number_format($amount) . ' USD = ' . number_format($converted) . ' IDR';
+//     echo '1 USD = ' . number_format($exchange_rate) . ' IDR';
+//     echo '<br>';
+//     echo number_format($amount) . ' USD = ' . number_format($converted) . ' IDR';
 
-});
+// });
