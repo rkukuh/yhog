32161e7523f4865ff982320c13b645ceee456427
R. Kukuh <rkukuh@gmail.com>
2018-05-09 11:41:44 +0700

    Construct dynamic donation bar

--- resources/views/front-end/common/interchange/goals-large.blade.php
+++ resources/views/front-end/common/interchange/goals-large.blade.php
@@ -1,6 +1,39 @@
 <div class="grid-x grid-padding-x">
 	<div class="cell">
 		<div class="grid-x medium-up-4 goals">
+			
+			@for ($i = 1; $i <= 4; $i++)
+				<div class="
+						cell goal 
+						@if ($progress >= ($i * 25)) on-progress @endif
+				">
+					@if ($progress >= ($i * 25)) 
+						<div class="bar" style="width: {{ $progress }}%"></div> 
+					@endif
+
+					<p>
+						@if ($i == 1)
+							0 IDR
+						@elseif ($i == 4)
+							{{ number_format($donation->target) }} IDR
+						@endif
+
+						<br>
+
+						<small>
+							{{ $i * 25 }} % Goal Reached
+						</small>
+					</p>
+				</div>
+			@endfor
+
+		</div>
+	</div>
+</div>
+
+<div class="grid-x grid-padding-x hide">
+	<div class="cell">
+		<div class="grid-x medium-up-4 goals">
 			<div class="cell goal reached">
 				<p>10.000.000 IDR<br><small>Goal Reached</small></p>
 			</div>
