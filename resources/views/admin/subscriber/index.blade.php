2cf225707ab102cc05ea25b7bac03f8532474831
R. Kukuh <rkukuh@gmail.com>
2018-05-28 14:51:51 +0700

    Rename terms

--- resources/views/admin/subscriber/index.blade.php
+++ resources/views/admin/subscriber/index.blade.php
@@ -8,21 +8,33 @@
     <div>
         <!-- Nav tabs -->
         <ul class="nav nav-tabs" role="tablist">
-            <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-            <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-            <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+            <li role="presentation" class="active">
+                <a href="#participant" aria-controls="Event Participant" role="tab" data-toggle="tab">
+                    Event Participant
+                </a>
+            </li>
+            <li role="presentation">
+                <a href="#donator" aria-controls="Donator" role="tab" data-toggle="tab">
+                    Donator
+                </a>
+            </li>
+            <li role="presentation">
+                <a href="#contact" aria-controls="Contact" role="tab" data-toggle="tab">
+                    Contact
+                </a>
+            </li>
         </ul>
 
         <!-- Tab panes -->
         <div class="tab-content">
-            <div role="tabpanel" class="tab-pane active" id="home">
-                home
+            <div role="tabpanel" class="tab-pane active" id="participant">
+                Event Participant...
             </div>
-            <div role="tabpanel" class="tab-pane" id="profile">
-                profile
+            <div role="tabpanel" class="tab-pane" id="donator">
+                Donator...
             </div>
-            <div role="tabpanel" class="tab-pane" id="messages">
-                message
+            <div role="tabpanel" class="tab-pane" id="contact">
+                Contact...
             </div>
         </div>
     </div>
