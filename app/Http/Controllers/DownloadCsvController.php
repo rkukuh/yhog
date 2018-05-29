da89ef09b9716b43749f3bbd29ed239a019cfb42
R. Kukuh <rkukuh@gmail.com>
2018-05-30 01:02:57 +0700

    Create controller to handle CSV generation

---
+++ app/Http/Controllers/DownloadCsvController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Participant;
+
+class DownloadCsvController extends Controller
+{
+    public function participant()
+    {
+        $filename = 'participants-' . Carbon::now()->format('Y-m-d-hi') . '.csv';
+        
+        $csvExporter = new \Laracsv\Export();
+        
+        $csvExporter->build(
+                        Participant::get(), 
+                        [
+                            'first_name', 
+                            'last_name',
+                            'email',
+                            'phone'
+                        ])
+                        ->download($filename);
+    }
+}
