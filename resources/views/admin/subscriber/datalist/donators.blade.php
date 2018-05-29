758bf24de2d84099a955dc435ab2bd847b693ab9
R. Kukuh <rkukuh@gmail.com>
2018-05-29 22:45:48 +0700

    Construct datalist

--- resources/views/admin/subscriber/datalist/donators.blade.php
+++ resources/views/admin/subscriber/datalist/donators.blade.php
@@ -1,1 +1,44 @@
-{{ $donators }}
+@if ($donators->isEmpty())
+    @include('common.datalist.no-data')
+@else
+    <table id="table" class="table table-bordered table-striped table-hover table-leveled">
+        <thead>
+            <tr>
+                <th class="text-center">Name</th>
+                <th class="text-center">Email</th>
+                <th class="text-center">Donation Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($donators as $donator)
+                <tr>
+                    <td>
+                        {{ $donator->full_name }} <br>
+
+                        on 
+                        <small class="text-muted">
+                            <a href="{{ route('admin.donation.show', $donator->donation) }}">
+                                {{ $donator->donation->title }}
+                            </a>
+                        </small>
+                    </td>
+                    <td>
+                        <i class="fa fa-envelope"></i> 
+                        <a href="mailto:{{ $donator->email }}">
+                            {{ $donator->email }}
+                        </a> 
+                    </td>
+                    <td class="text-right">
+                        <span>
+                            {{ $donator->amount_formatted }} 
+                        </span>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <div class="pull-right">
+        {{ $donators->links() }}
+    </div>
+@endif
